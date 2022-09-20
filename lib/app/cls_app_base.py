18ae76dd9684b1cf1be6b8ea2902a80e5ad3ee4b
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-09-20 20:10:52 +0900

    Create cls_app_base.py

---
+++ lib/app/cls_app_base.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import tkinter as tk
+import cv2
+from lib.cls_average import Average
+
+
+class App_Base:
+    def __init__(self, master=None):
+        # build ui
+        self.toplevel1 = tk.Tk() if master is None else tk.Toplevel(master)
+        self.toplevel1.configure(height=200, width=200)
+        self.frame1 = tk.Frame(self.toplevel1)
+        self.frame1.configure(height=200, padx=5, pady=5, width=200)
+        self.tkvar = tk.StringVar()
+        __values = ['aaa', 'bbb']
+        self.optionmenu1 = tk.OptionMenu(
+            self.frame1, self.tkvar, *__values, command=None)
+        self.optionmenu1.pack(fill="x", side="top")
+        self.frame3 = tk.Frame(self.frame1)
+        self.frame3.configure(height=200, width=200)
+        self.add_btn = tk.Button(self.frame3)
+        self.add_btn.configure(text="Add")
+        self.add_btn.pack(expand="true", fill="x", side="left")
+        self.del_btn = tk.Button(self.frame3)
+        self.del_btn.configure(text="Del")
+        self.del_btn.pack(expand="true", fill="x", side="left")
+        self.frame3.pack(fill="x", side="top")
+        self.task_list = tk.Listbox(self.frame1)
+        self.task_list.pack(expand="true", fill="both", side="top")
+        self.create_code_btn = tk.Button(self.frame1)
+        self.create_code_btn.configure(text="Create Python Code")
+        self.create_code_btn.pack(fill="x", side="top")
+        self.frame1.pack(fill="y", side="left")
+        self.dummy_frame = tk.Frame(self.toplevel1)
+        self.dummy_frame.configure(height=200, padx=5, pady=5, width=200)
+        self.dummy_frame.pack(expand="true", fill="both", side="left")
+
+        # Main widget
+        self.appwindow = self.toplevel1
+
+    def run(self):
+        self.appwindow.mainloop()
+
+
+if __name__ == "__main__":
+    img = cv2.imread('./0000_img/opencv_logo.jpg')
+
+    app = App_Base()
+    param = []
+    param = [15, 15]
+    app.dummy_frame.destroy()
+    app_child = Average(img, param, master=app.appwindow, gui=True)
+    # app_child.image_edit_frame.destroy()
+    # app_child = Average(img, param, master=app.mainwindow, gui=True)
+    # app_child.image_edit_frame.destroy()
+    app.run()
