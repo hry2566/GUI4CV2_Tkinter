573b29581875c422bdcc0b37af8464b8f4e654ec
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-09-24 03:18:44 +0900

    Create cls_gaussian_blur.py

---
+++ lib/cls_gaussian_blur.py
@@ -0,0 +1,99 @@
+import tkinter as tk
+
+import cv2
+
+from lib.gui.cls_edit_window import EditWindow, even2odd
+
+
+class Gaussian_Blur(EditWindow):
+    def __init__(self, img, param, master=None, gui=False):
+        self.origin_img = img
+        self.__proc_flag = False
+
+        if len(param) == 3:
+            self.__kernel_x = param[0]
+            self.__kernel_y = param[1]
+            self.__std = param[2]
+        else:
+            self.__kernel_x = 1
+            self.__kernel_y = 1
+            self.__std = 1
+
+        if gui:
+            super().__init__(img, master)
+            self.__init_gui()
+            self.__init_events()
+
+        self.dst_img = self.__gaussian_blur()
+
+        if gui:
+            self.run()
+
+    def __init_gui(self):
+        self.none_label.destroy()
+
+        self.__scale_x = tk.Scale(self.settings_frame)
+        self.__scale_x.configure(from_=1, to=50,
+                                 label="kernel x", orient="horizontal", command=self.__onScale)
+        self.__scale_x.pack(side="top")
+
+        self.__scale_y = tk.Scale(self.settings_frame)
+        self.__scale_y.configure(from_=1, to=50,
+                                 label="kernel y", orient="horizontal", command=self.__onScale)
+        self.__scale_y.pack(side="top")
+
+        self.__scale_std = tk.Scale(self.settings_frame)
+        self.__scale_std.configure(from_=1, to=50,
+                                   label="std", orient="horizontal", command=self.__onScale)
+        self.__scale_std.pack(side="top")
+
+        self.__scale_x.set(self.__kernel_x)
+        self.__scale_y.set(self.__kernel_y)
+        self.__scale_std.set(self.__std)
+        pass
+
+    def __init_events(self):
+        pass
+
+    def __onScale(self, events):
+        if self.__proc_flag:
+            return
+        else:
+            self.__proc_flag = True
+
+        self.__kernel_x = self.__scale_x.get()
+        self.__kernel_y = self.__scale_y.get()
+        self.__std = self.__scale_std.get()
+        self.dst_img = self.__gaussian_blur()
+        self.Draw()
+        self.__proc_flag = False
+        pass
+
+    def __gaussian_blur(self):
+        img_copy = self.origin_img.copy()
+
+        self.__kernel_x = even2odd(self.__kernel_x)
+        self.__kernel_y = even2odd(self.__kernel_y)
+
+        img = cv2.GaussianBlur(img_copy,
+                               (self.__kernel_x, self.__kernel_y),
+                               self.__std)
+        return img
+
+    def get_data(self):
+        param = []
+        param.append(self.__kernel_x)
+        param.append(self.__kernel_y)
+        param.append(self.__std)
+        print('Proc : Gaussian Blur')
+        print(f'param = {param}')
+        return param, self.dst_img
+
+
+if __name__ == "__main__":
+    img = cv2.imread('./0000_img/opencv_logo.jpg')
+    param = []
+    param = [33, 33, 3]
+    app = Gaussian_Blur(img, param, gui=True)
+    param, dst_img = app.get_data()
+    cv2.imwrite('./Gaussian_Blur.jpg', dst_img)
