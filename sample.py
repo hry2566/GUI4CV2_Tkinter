a055edfebc0a5a8701068322087670d132eed504
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-09-25 09:50:50 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,10 @@
-
 import cv2
 from lib.cls_average import Average
 
 img = cv2.imread('./0000_img/opencv_logo.jpg')
-param = []
-average = Average(img, param, gui=True)
+
+param = [15, 15]
+average = Average(img, param, gui=False)
 param, dst_img = average.get_data()
+
+cv2.imwrite('test.jpg', dst_img)
