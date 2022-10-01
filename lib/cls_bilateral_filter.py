f9c6fed900fbb3f4abb771714ea0906a97fbc663
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-10-01 10:01:19 +0900

    Update cls_bilateral_filter.py

--- lib/cls_bilateral_filter.py
+++ lib/cls_bilateral_filter.py
@@ -11,13 +11,13 @@ class Bilateral_Filter(EditWindow):
         self.__proc_flag = False
 
         if len(param) == 3:
-            self.d = param[0]
-            self.sigma_color = param[1]
-            self.sigma_space = param[2]
+            self.__d = param[0]
+            self.__sigma_color = param[1]
+            self.__sigma_space = param[2]
         else:
-            self.d = 0
-            self.sigma_color = 0
-            self.sigma_space = 0
+            self.__d = 0
+            self.__sigma_color = 0
+            self.__sigma_space = 0
 
         if gui:
             super().__init__(img, master)
@@ -32,22 +32,22 @@ class Bilateral_Filter(EditWindow):
     def __init_gui(self):
         self.none_label.destroy()
 
-        self.scale1 = tk.Scale(self.settings_frame)
-        self.scale1.configure(from_=-128, to=128,
-                              label="d", orient="horizontal", command=self.__onScale)
-        self.scale1.pack(side="top")
-        self.scale2 = tk.Scale(self.settings_frame)
-        self.scale2.configure(from_=0, to=30,
-                              label="sigma_color", orient="horizontal", command=self.__onScale)
-        self.scale2.pack(side="top")
-        self.scale3 = tk.Scale(self.settings_frame)
-        self.scale3.configure(from_=0, to=30,
-                              label="sigma_space", orient="horizontal", command=self.__onScale)
-        self.scale3.pack(side="top")
+        self.__scale1 = tk.Scale(self.settings_frame)
+        self.__scale1.configure(from_=-128, to=128,
+                                label="d", orient="horizontal", command=self.__onScale)
+        self.__scale1.pack(side="top")
+        self.__scale2 = tk.Scale(self.settings_frame)
+        self.__scale2.configure(from_=0, to=50,
+                                label="sigma_color", orient="horizontal", command=self.__onScale)
+        self.__scale2.pack(side="top")
+        self.__scale3 = tk.Scale(self.settings_frame)
+        self.__scale3.configure(from_=0, to=50,
+                                label="sigma_space", orient="horizontal", command=self.__onScale)
+        self.__scale3.pack(side="top")
 
-        self.scale1.set(self.d)
-        self.scale2.set(self.sigma_color)
-        self.scale3.set(self.sigma_space)
+        self.__scale1.set(self.__d)
+        self.__scale2.set(self.__sigma_color)
+        self.__scale3.set(self.__sigma_space)
         pass
 
     def __init_events(self):
@@ -59,9 +59,9 @@ class Bilateral_Filter(EditWindow):
         else:
             self.__proc_flag = True
 
-        self.d = self.scale1.get()
-        self.sigma_color = self.scale2.get()
-        self.sigma_space = self.scale3.get()
+        self.__d = self.__scale1.get()
+        self.__sigma_color = self.__scale2.get()
+        self.__sigma_space = self.__scale3.get()
 
         self.dst_img = self.__bilateral_filter()
         self.Draw()
@@ -72,16 +72,16 @@ class Bilateral_Filter(EditWindow):
         img_copy = self.origin_img.copy()
 
         img = cv2.bilateralFilter(img_copy,
-                                  d=self.d,
-                                  sigmaColor=self.sigma_color,
-                                  sigmaSpace=self.sigma_space)
+                                  d=self.__d,
+                                  sigmaColor=self.__sigma_color,
+                                  sigmaSpace=self.__sigma_space)
         return img
 
     def get_data(self):
         param = []
-        param.append(self.d)
-        param.append(self.sigma_color)
-        param.append(self.sigma_space)
+        param.append(self.__d)
+        param.append(self.__sigma_color)
+        param.append(self.__sigma_space)
         print('Proc : Bilateral_Filter')
         print(f'param = {param}')
         return param, self.dst_img
