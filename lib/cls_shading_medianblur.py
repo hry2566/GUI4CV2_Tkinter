ffce2a170d23f82fe2a4ad3d4ed9675ee28b0559
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-10-17 15:22:26 +0900

    Update cls_shading_medianblur.py

--- lib/cls_shading_medianblur.py
+++ lib/cls_shading_medianblur.py
@@ -11,25 +11,21 @@ class Shading_MedianBlur(EditWindow):
         self.origin_img = img
         self.__kernel = 1
         self.__noise_cut = 0
+        self.__select_menu = 0
         self.__proc_flag = False
 
-        if len(param) == 2:
+        if len(param) == 3:
             self.__kernel = param[0]
             self.__noise_cut = param[1]
-        else:
-            self.__kernel = 1
-            self.__noise_cut = 0
+            self.__select_menu = param[2]
 
         if gui:
             super().__init__(img, master)
             self.__init_gui()
             self.__init_events()
-
-        self.dst_img = self.__shading_median_blur()
-
-        if gui:
-            self.Draw()
             self.run()
+        else:
+            self.dst_img = self.__shading_median_blur()
 
     def __init_gui(self):
         self.none_label.destroy()
@@ -40,14 +36,27 @@ class Shading_MedianBlur(EditWindow):
         self.__scale1.pack(side='top')
 
         self.__scale3 = tk.Scale(self.settings_frame)
-        self.__scale3.configure(from_=0, to=255,
+        self.__scale3.configure(from_=0, to=128,
                                 label='remove noise', orient='horizontal', command=self.__onScale)
         self.__scale3.pack(side='top')
+
+        self.__tkvar = tk.StringVar(value=None)
+        __values = ['明', '暗', '明＋暗']
+        self.optionmenu1 = tk.OptionMenu(
+            self.settings_frame, self.__tkvar, 'None', *__values, command=self.__onSelect)
+        self.optionmenu1.pack(side='top', fill='x')
 
         self.__scale1.set(self.__kernel)
         self.__scale3.set(self.__noise_cut)
 
-        pass
+        if self.__select_menu == 0:
+            self.__tkvar.set(None)
+        elif self.__select_menu == 1:
+            self.__tkvar.set('明')
+        elif self.__select_menu == 2:
+            self.__tkvar.set('暗')
+        elif self.__select_menu == 3:
+            self.__tkvar.set('明＋暗')
 
     def __init_events(self):
         pass
@@ -64,6 +73,19 @@ class Shading_MedianBlur(EditWindow):
         self.Draw()
         self.__proc_flag = False
 
+    def __onSelect(self, event):
+        if self.__tkvar.get() == 'None':
+            self.__select_menu = 0
+        elif self.__tkvar.get() == '明':
+            self.__select_menu = 1
+        elif self.__tkvar.get() == '暗':
+            self.__select_menu = 2
+        elif self.__tkvar.get() == '明＋暗':
+            self.__select_menu = 3
+
+        self.dst_img = self.__shading_median_blur()
+        self.Draw()
+
     def __shading_median_blur(self):
         img_copy = self.origin_img.copy()
         image = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
@@ -74,11 +96,17 @@ class Shading_MedianBlur(EditWindow):
         img = image/blur
         img = np.clip(img*128, 0, 255).astype(np.uint8)
 
-        if self.__noise_cut > 0:
+        if not self.__select_menu == 0:
             for index in range(height):
-                y1 = img[index:index+1, 0:width][0]
-                y1 = np.where(abs(y1) < self.__noise_cut, 255, int(255/2))
-                img[index:index+1, 0:width][0] = y1
+                v = img[index:index+1, 0:width][0]
+                if self.__select_menu == 1:  # 明
+                    v = np.where((v < 128+self.__noise_cut),  0, 255)
+                elif self.__select_menu == 2:  # 暗
+                    v = np.where((v > 128-self.__noise_cut),  255, 0)
+                elif self.__select_menu == 3:  # 明暗
+                    v = np.where((v > 128-self.__noise_cut) &
+                                 (v < 128+self.__noise_cut),  int(255/2), 255)
+                img[index:index+1, 0:width][0] = v
 
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
         return img
@@ -87,6 +115,7 @@ class Shading_MedianBlur(EditWindow):
         param = []
         param.append(self.__kernel)
         param.append(self.__noise_cut)
+        param.append(self.__select_menu)
         print('Proc : Shading_MedianBlur')
         print(f'param = {param}')
         return param, self.dst_img
@@ -94,8 +123,9 @@ class Shading_MedianBlur(EditWindow):
 
 if __name__ == "__main__":
     img = cv2.imread('./0000_img/shading.png')
-    # img = cv2.imread('./0000_img/I.jpg')
+    # img = cv2.imread('./0000_img/10.png')
     param = []
+    param = [18, 38, 2]
     app = Shading_MedianBlur(img, param, gui=True)
     param, dst_img = app.get_data()
     cv2.imwrite('./Shading_MedianBlur.jpg', dst_img)
