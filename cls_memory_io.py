73bf1b45fe205d584625d415979d893939af8355
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-10-21 16:07:30 +0900

    Create cls_memory_io.py

---
+++ cls_memory_io.py
@@ -0,0 +1,250 @@
+import tkinter as tk
+
+import cv2
+import numpy as np
+from lib.cls_create_img_memory import Create_Img_Memory
+from lib.cls_open_file import OpenFile
+
+from lib.gui.cls_edit_window import EditWindow
+
+
+class MemoryIO(EditWindow):
+    def __init__(self, img, param, master=None, gui=False):
+        self.origin_img = img
+        self.img_bk = img
+        self.__img_array = []
+        self.__img_names = []
+        self.__memIO = []
+        # self.__proc_flag = False
+
+        if type(self.origin_img) == list:
+            img = self.origin_img[0]
+
+        if len(param) == 3:
+            self.__img_array = param[0]
+            self.__img_names = param[1]
+            self.__memIO = param[2]
+            pass
+        else:
+            exit()
+
+        if gui:
+            super().__init__(img, master)
+            self.__init_gui()
+            self.__init_events()
+        else:
+            self.dst_img = self.__memory_io()
+
+        if gui:
+            self.Draw()
+            self.run()
+
+    def __init_gui(self):
+        self.none_label.destroy()
+
+        self.frame1 = tk.Frame(self.settings_frame)
+        self.frame3 = tk.Frame(self.frame1)
+        self.frame6 = tk.Frame(self.frame3)
+        self.__tkvar = tk.StringVar(value='source_IMG')
+        __values = []
+        for name in self.__img_names:
+            __values.append(name)
+        self.optionmenu1 = tk.OptionMenu(
+            self.frame6, self.__tkvar, self.__tkvar.get(), *__values, command=self.__onSelectedMenu)
+        self.optionmenu1.pack(fill='x', side='top')
+        self.listbox1 = tk.Listbox(self.frame6)
+        self.listbox1.pack(expand='true', fill='both', side='top')
+        self.frame6.configure(height='200', width='200')
+        self.frame6.pack(expand='true', fill='both', side='left')
+        self.frame7 = tk.Frame(self.frame3)
+        self.button2 = tk.Button(self.frame7)
+        self.button2.configure(text='add', command=self.__onBtn2Click)
+        self.button2.pack(fill='x', side='top')
+        self.button3 = tk.Button(self.frame7)
+        self.button3.configure(text='del', command=self.__onBtn3Click)
+        self.button3.pack(fill='x', side='top')
+        self.frame7.configure(height='200', width='200')
+        self.frame7.pack(side='left')
+        self.frame9 = tk.Frame(self.frame3)
+        self.label1 = tk.Label(self.frame9)
+        self.label1.pack(pady='5', side='top')
+        self.listbox2 = tk.Listbox(self.frame9)
+        self.listbox2.pack(expand='true', fill='both', side='top')
+        self.frame9.configure(height='200', width='200')
+        self.frame9.pack(expand='true', fill='both', side='left')
+        self.frame3.configure(height='200', width='200')
+        self.frame3.pack(expand='true', fill='both', side='top')
+        self.frame1.configure(height='200', width='200')
+        self.frame1.pack(expand='true', fill='both', side='top')
+        self.frame2 = tk.Frame(self.settings_frame)
+        self.button1 = tk.Button(self.frame2)
+        self.button1.configure(text='exec', command=self.__onBtn1Click)
+        self.button1.pack(fill='x', side='top')
+        self.frame2.configure(height='200', width='200')
+        self.frame2.pack(fill='x', side='top')
+
+        if type(self.img_bk) == np.ndarray:
+            self.listbox2.insert(tk.END, 'source_IMG')
+        else:
+            for cnt, _ in enumerate(self.img_bk):
+                self.listbox2.insert(tk.END, f'source_IMG[{cnt}]')
+        for name in self.__img_names:
+            self.listbox2.insert(tk.END, name)
+
+        self.listbox2.select_set(0)
+
+        if not len(self.__memIO) == 0:
+            if self.__memIO[0] == '':
+                self.__tkvar.set('source_IMG')
+                self.__memIO[0] = 'source_IMG'
+            else:
+                self.__tkvar.set(self.__memIO[0])
+
+            for name in self.__memIO[1]:
+                self.listbox1.insert(tk.END, name)
+
+    def __init_events(self):
+        self.listbox2.bind('<<ListboxSelect>>', self.__onLst2Select)
+        pass
+
+    def __onScale(self, events):
+        pass
+
+    def __onSelectedMenu(self, event):
+        self.__memIO[0] = self.__tkvar.get()
+
+        while(True):
+            if self.listbox1.get(0) == '':
+                break
+            self.listbox1.delete(0)
+
+    def __onBtn1Click(self):
+        self.dst_img = self.__memory_io()
+        pass
+
+    def __onBtn2Click(self):
+        if self.listbox2.curselection() == ():
+            return
+
+        input_name = self.__tkvar.get()
+        if not input_name.startswith('source_IMG'):
+            index = self.listbox2.curselection()[0]
+            name = self.listbox2.get(index)
+            if self.listbox1.get(0) == '' and name.startswith('source_IMG'):
+                self.__addLst1()
+        else:
+            self.__addLst1()
+
+    def __addLst1(self):
+        index = self.listbox2.curselection()[0]
+        name = self.listbox2.get(index)
+        flag = False
+        for index in range(10000):
+            lst1_name = self.listbox1.get(index)
+            if lst1_name == '':
+                break
+            if lst1_name == name:
+                flag = True
+                break
+
+        if not flag:
+            self.listbox1.insert(tk.END, name)
+            self.__memIO[1].append(name)
+
+    def __onBtn3Click(self):
+        if self.listbox1.curselection() == ():
+            return
+
+        index = self.listbox1.curselection()[0]
+        self.listbox1.delete(index)
+        self.__memIO[1].pop(index)
+
+    def __onLst2Select(self, event):
+        if self.listbox2.curselection() == ():
+            return
+
+        # index = event.widget.curselection()[0]
+        index = self.listbox2.curselection()[0]
+        name = self.listbox2.get(index)
+        name_index = -1
+        if name.startswith('source_IMG['):
+            name_index = int(name.replace('source_IMG[', '').replace(']', ''))
+            self.dst_img = self.img_bk[name_index]
+        else:
+            if name.startswith('source_IMG'):
+                self.dst_img = self.img_bk
+            else:
+                name_index = self.__img_names.index(name)
+                self.dst_img = self.__img_array[name_index]
+        self.Draw()
+
+    def __memory_io(self):
+        img = self.img_bk.copy()
+        self.img_dummy = []
+
+        menu_val = self.__memIO[0]
+        if menu_val == 'source_IMG':
+            cnt = 0
+
+            for cnt in range(len(self.__memIO[1])):
+                name = self.__memIO[1][cnt]
+                if name == '':
+                    break
+                if name.startswith('source_IMG['):
+                    index = int(name.replace(
+                        'source_IMG[', '').replace(']', ''))
+                    self.img_dummy.append(self.img_bk[index])
+                else:
+                    index = self.__img_names.index(name)
+                    self.img_dummy.append(self.__img_array[index])
+                cnt += 1
+        else:
+            in_name = self.__memIO[0]
+            if in_name.startswith('source_IMG['):
+                name_index = int(in_name.replace(
+                    'source_IMG[', '').replace(']', ''))
+                self.__img_array[self.__img_names.index(
+                    menu_val)] = self.img_bk[name_index]
+            else:
+                self.__img_array = []
+                self.__img_array.append(img)
+
+        if type(self.img_dummy) == list:
+            if len(self.img_dummy) == 1:
+                img = self.img_dummy[0]
+            else:
+                img = self.img_dummy
+
+        return img
+
+    def get_data(self):
+        param = []
+        param = [self.__img_array, self.__img_names, self.__memIO]
+        print('Proc : MemoryIO')
+        print(f'param = {param[2]}')
+        return param, self.img_bk
+
+
+if __name__ == "__main__":
+    param = ['C:/Users/2566haraya/Desktop/GitHub/OpenCV-GUI/0000_img/opencv_logo2.jpg']
+    imgLib = OpenFile(param, gui=False)
+    param, img = imgLib.get_data()
+
+    img_array = []
+    img_names = ['img1', 'img2']
+    param = [img_array, img_names]
+    imgLib = Create_Img_Memory(img, [img_array, img_names], gui=False)
+    param, img = imgLib.get_data()
+    img_array = param[0]
+    img_names = param[1]
+
+    memIO = ['img1', ['source_IMG']]
+    param = [img_array, img_names, memIO]
+    imgLib = MemoryIO(img, param, gui=False)
+    param, img = imgLib.get_data()
+    img_array = param[0]
+    img_names = param[1]
+
+    cv2.imwrite('./MemoryIO1.jpg', img_array[0])
+
+    
