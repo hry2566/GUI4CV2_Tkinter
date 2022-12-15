9f2d33e4888dfd8ee1b4f7dce4269833290a66e1
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-15 15:31:33 +0900

    Update cls_inrange.py

--- lib/cls_inrange.py
+++ lib/cls_inrange.py
@@ -3,266 +3,227 @@ import numpy as np
 import cv2
 
 from lib.gui.cls_edit_window import EditWindow
+from lib.parts.parts_scale import Parts_Scale
 
 
 class InRange(EditWindow):
     def __init__(self, img, param, master=None, gui=False):
         self.origin_img = img
-        self.__origin_bk = img
+        # self.__origin_bk = img
         self.__proc_flag = False
-        self.__r_h_1 = 0
-        self.__g_s_1 = 0
-        self.__b_v_1 = 0
-        self.__r_h_2 = 255
-        self.__g_s_2 = 255
-        self.__b_v_2 = 255
-        self.__hsv_flag = False
-        self.__start_x = 0
-        self.__start_y = 0
-        self.__drag_flag = False
-        self.__add_flag = False
-        self.__select_img = []
-        self.__master = master
+        self.__P1min = 0
+        self.__P1max = 255
+        self.__P2min = 0
+        self.__P2max = 255
+        self.__P3min = 0
+        self.__P3max = 255
+        self.__flag = False
+        # self.__master = master
 
         if len(param) == 7:
-            self.__r_h_1 = param[0]
-            self.__g_s_1 = param[1]
-            self.__b_v_1 = param[2]
-            self.__r_h_2 = param[3]
-            self.__g_s_2 = param[4]
-            self.__b_v_2 = param[5]
-            self.__hsv_flag = param[6]
+            self.__P1min = param[0]
+            self.__P1max = param[1]
+            self.__P2min = param[2]
+            self.__P2max = param[3]
+            self.__P3min = param[4]
+            self.__P3max = param[5]
+            self.__flag = param[6]
+            pass
 
         if gui:
             super().__init__(img, master)
             self.__init_gui()
             self.__init_events()
+
+        self.dst_img = self.__inrange()
+
+        if gui:
+            self.Draw()
             self.run()
-        else:
-            self.dst_img = self.__inrange()
 
     def __init_gui(self):
         self.none_label.destroy()
 
-        self.__hsv_bool = tk.BooleanVar()
-        self.__hsv_bool.set(self.__hsv_flag)
+        self.var = tk.IntVar()
 
-        self.checkbutton1 = tk.Checkbutton(
-            self.settings_frame, variable=self.__hsv_bool)
-        self.checkbutton1.configure(text="HSV", command=self.__onClick)
-        self.checkbutton1.pack(anchor="w", side="top")
+        self.option_frame = tk.Frame(self.settings_frame)
+        self.option_frame.configure(height=200, width=200)
+        self.radiobutton1 = tk.Radiobutton(
+            self.option_frame, value=0, variable=self.var)
+        self.radiobutton1.configure(text='RGB')
+        self.radiobutton1.pack(expand="true", fill="x", side="left")
+        self.radiobutton2 = tk.Radiobutton(
+            self.option_frame, value=1, variable=self.var)
+        self.radiobutton2.configure(text='HSV')
+        self.radiobutton2.pack(expand="true", fill="x", side="left")
+        self.option_frame.pack(fill="x", side="top")
+        self.rgb_frame = tk.LabelFrame(self.settings_frame)
+        self.rgb_frame.configure(height=200, text='RGB', width=200)
+        self.scale_Rmin = Parts_Scale(self.rgb_frame)
+        self.scale_Rmin.configure(label='R_min', side='top', from_=0, to=255)
+        self.scale_Rmax = Parts_Scale(self.rgb_frame)
+        self.scale_Rmax.configure(label='R_max', side='top', from_=0, to=255)
+        self.scale_Gmin = Parts_Scale(self.rgb_frame)
+        self.scale_Gmin.configure(label='G_min', side='top', from_=0, to=255)
+        self.scale_Gmax = Parts_Scale(self.rgb_frame)
+        self.scale_Gmax.configure(label='G_max', side='top', from_=0, to=255)
+        self.scale_Bmin = Parts_Scale(self.rgb_frame)
+        self.scale_Bmin.configure(label='B_min', side='top', from_=0, to=255)
+        self.scale_Bmax = Parts_Scale(self.rgb_frame)
+        self.scale_Bmax.configure(label='B_max', side='top', from_=0, to=255)
+        self.rgb_frame.pack(fill="x", side="top")
+        self.hsv_frame = tk.LabelFrame(self.settings_frame)
+        self.hsv_frame.configure(height=200, text='HSV', width=200)
+        self.scale_Hmin = Parts_Scale(self.hsv_frame)
+        self.scale_Hmin.configure(label='H_min', side='top', from_=0, to=255)
+        self.scale_Hmax = Parts_Scale(self.hsv_frame)
+        self.scale_Hmax.configure(label='H_max', side='top', from_=0, to=255)
+        self.scale_Smin = Parts_Scale(self.hsv_frame)
+        self.scale_Smin.configure(label='S_min', side='top', from_=0, to=255)
+        self.scale_Smax = Parts_Scale(self.hsv_frame)
+        self.scale_Smax.configure(label='S_max', side='top', from_=0, to=255)
+        self.scale_Vmin = Parts_Scale(self.hsv_frame)
+        self.scale_Vmin.configure(label='V_min', side='top', from_=0, to=255)
+        self.scale_Vmax = Parts_Scale(self.hsv_frame)
+        self.scale_Vmax.configure(label='V_max', side='top', from_=0, to=255)
+        self.hsv_frame.pack(side="top")
 
-        self.__scale1 = tk.Scale(self.settings_frame)
-        self.__scale1.configure(from_=0, to=255,
-                                label='r(v)_1', orient="horizontal", command=self.__onScale)
-        self.__scale1.pack(side="top")
-
-        self.__scale2 = tk.Scale(self.settings_frame)
-        self.__scale2.configure(from_=0, to=255,
-                                label='g(s)_1', orient="horizontal", command=self.__onScale)
-        self.__scale2.pack(side="top")
-
-        self.__scale3 = tk.Scale(self.settings_frame)
-        self.__scale3.configure(from_=0, to=255,
-                                label='b(h)_1', orient="horizontal", command=self.__onScale)
-        self.__scale3.pack(side="top")
-
-        self.__scale4 = tk.Scale(self.settings_frame)
-        self.__scale4.configure(from_=0, to=255,
-                                label='r(v)_2', orient="horizontal", command=self.__onScale)
-        self.__scale4.pack(side="top")
-
-        self.__scale5 = tk.Scale(self.settings_frame)
-        self.__scale5.configure(from_=0, to=255,
-                                label='g(s)_2', orient="horizontal", command=self.__onScale)
-        self.__scale5.pack(side="top")
-
-        self.__scale6 = tk.Scale(self.settings_frame)
-        self.__scale6.configure(from_=0, to=255,
-                                label='b(h)_2', orient="horizontal", command=self.__onScale)
-        self.__scale6.pack(side="top")
-
-        self.__scale1.set(self.__r_h_1)
-        self.__scale2.set(self.__g_s_1)
-        self.__scale3.set(self.__b_v_1)
-        self.__scale4.set(self.__r_h_2)
-        self.__scale5.set(self.__g_s_2)
-        self.__scale6.set(self.__b_v_2)
-
-        self.reset_label = tk.Label(self.settings_frame)
-        self.reset_label.configure(text="select reset")
-        self.reset_label.pack(side="top", fill='x')
-
-        self.add_reset_btn = tk.Button(self.settings_frame)
-        self.add_reset_btn.configure(text="reset", command=self.__select_reset)
-        self.add_reset_btn.pack(side="top", fill='x')
-
-    def __onClick(self):
-        self.__hsv_flag = self.__hsv_bool.get()
-        self.dst_img = self.__inrange()
-        self.Draw()
+        if not self.__flag:
+            self.__show_mode(0)
+            self.var.set(0)
+            self.scale_Rmin.set(self.__P1min)
+            self.scale_Rmax.set(self.__P1max)
+            self.scale_Gmin.set(self.__P2min)
+            self.scale_Gmax.set(self.__P2max)
+            self.scale_Bmin.set(self.__P3min)
+            self.scale_Bmax.set(self.__P3max)
+            self.scale_Hmax.set(255)
+            self.scale_Smax.set(255)
+            self.scale_Vmax.set(255)
+        else:
+            self.__show_mode(1)
+            self.var.set(1)
+            self.scale_Hmin.set(self.__P1min)
+            self.scale_Hmax.set(self.__P1max)
+            self.scale_Smin.set(self.__P2min)
+            self.scale_Smax.set(self.__P2max)
+            self.scale_Vmin.set(self.__P3min)
+            self.scale_Vmax.set(self.__P3max)
+            self.scale_Rmax.set(255)
+            self.scale_Gmax.set(255)
+            self.scale_Bmax.set(255)
 
     def __init_events(self):
-        self.canvas1.bind("<ButtonPress-1>", self.__OnMouseDown)
-        self.canvas1.bind("<ButtonRelease-1>", self.__OnMouseUp)
-        self.canvas1.bind('<B1-Motion>', self.__OnMouseMove)
+        self.radiobutton1.configure(command=self.__onClick_optbtn)
+        self.radiobutton2.configure(command=self.__onClick_optbtn)
+        self.scale_Rmin.bind(changed=self.__onScale)
+        self.scale_Rmax.bind(changed=self.__onScale)
+        self.scale_Gmin.bind(changed=self.__onScale)
+        self.scale_Gmax.bind(changed=self.__onScale)
+        self.scale_Bmin.bind(changed=self.__onScale)
+        self.scale_Bmax.bind(changed=self.__onScale)
 
-        if self.__master == None:
-            self.mainwindow.bind("<KeyPress>", self.__keydown_event)
-            self.mainwindow.bind("<KeyRelease>", self.__keyup_event)
+        self.scale_Hmin.bind(changed=self.__onScale)
+        self.scale_Hmax.bind(changed=self.__onScale)
+        self.scale_Smin.bind(changed=self.__onScale)
+        self.scale_Smax.bind(changed=self.__onScale)
+        self.scale_Vmin.bind(changed=self.__onScale)
+        self.scale_Vmax.bind(changed=self.__onScale)
+
+    def __onClick_optbtn(self):
+        mode = self.var.get()
+        if mode == 0:
+            self.__flag = False
         else:
-            master = self.settings_frame.master.master.master
-            master.bind("<KeyPress>", self.__keydown_event)
-            master.bind("<KeyRelease>", self.__keyup_event)
-        pass
-
-    def __keydown_event(self, event):
-        if event.keysym == 'Control_L':
-            self.__add_flag = True
-
-    def __keyup_event(self, event):
-        pass
-
-    def __select_reset(self):
-        self.__select_img = []
-
-    def __GetMousePos(self, pos):
-        view_scale = self.GetViewScale()
-        canvas_width = int(self.canvas1.winfo_width()*view_scale)
-        canvas_height = int(self.canvas1.winfo_height()*view_scale)
-
-        img_width = self.origin_img.shape[1]
-        img_height = self.origin_img.shape[0]
-
-        scale_x = img_width/canvas_width
-        scale_y = img_height/canvas_height
-        pos_x, pos_y = self.GetImgPos()
-
-        if scale_x < scale_y:
-            scale = scale_y
-            dev = int((canvas_width-img_width/scale)/2)
-            x = int((pos.x-dev)*scale)-int(pos_x*scale)
-            y = int(pos.y*scale)-int(pos_y*scale)
-        else:
-            scale = scale_x
-            dev = int((canvas_height-img_height/scale)/2)
-            x = int(pos.x*scale)-int(pos_x*scale)
-            y = int((pos.y-dev)*scale)-int(pos_y*scale)
-
-        return x, y
-
-    def __OnMouseDown(self, event):
-        if not self.__add_flag:
-            return
-
-        self.__drag_flag = True
-        self.__start_x, self.__start_y = self.__GetMousePos(event)
-
-    def __OnMouseUp(self, event):
-        if not self.__add_flag:
-            return
-
-        self.__add_flag = False
-        self.__drag_flag = False
-        self.origin_img = self.__origin_bk.copy()
-        x, y = self.__GetMousePos(event)
-
-        if x == self.__start_x and y == self.__start_y:
-            return
-
-        if self.__start_x > x:
-            dummy1 = self.__start_x
-            dummy2 = x
-            self.__start_x = dummy2
-            x = dummy1
-
-        if self.__start_y > y:
-            dummy1 = self.__start_y
-            dummy2 = y
-            self.__start_y = dummy2
-            y = dummy1
-
-        img1 = self.origin_img[self.__start_y:y, self.__start_x:x]
-        self.__select_img.append(img1)
-
-        total_b_h = []
-        total_g_s = []
-        total_r_v = []
-        for img in self.__select_img:
-            if self.__hsv_flag:
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
-
-            b_h, g_s, r_v = cv2.split(img)
-            total_b_h.extend(b_h[0].tolist())
-            total_g_s.extend(g_s[0].tolist())
-            total_r_v.extend(r_v[0].tolist())
-
-        self.__proc_flag = True
-        self.__scale1.set(min(total_r_v))
-        self.__scale2.set(min(total_g_s))
-        self.__scale3.set(min(total_b_h))
-        self.__scale4.set(max(total_r_v))
-        self.__scale5.set(max(total_g_s))
-        self.__scale6.set(max(total_b_h))
-        self.__proc_flag = False
-
+            self.__flag = True
+        self.__show_mode(mode)
+        self.__get_param_val(mode)
+        self.dst_img = self.__inrange()
         self.Draw()
 
-    def __OnMouseMove(self, event):
-        if not self.__add_flag:
-            return
+    def __get_param_val(self, mode):
+        if mode == 0:
+            self.__P1min = self.scale_Rmin.get()
+            self.__P1max = self.scale_Rmax.get()
+            self.__P2min = self.scale_Gmin.get()
+            self.__P2max = self.scale_Gmax.get()
+            self.__P3min = self.scale_Bmin.get()
+            self.__P3max = self.scale_Bmax.get()
+        else:
+            self.__P1min = self.scale_Hmin.get()
+            self.__P1max = self.scale_Hmax.get()
+            self.__P2min = self.scale_Smin.get()
+            self.__P2max = self.scale_Smax.get()
+            self.__P3min = self.scale_Vmin.get()
+            self.__P3max = self.scale_Vmax.get()
 
-        if self.__drag_flag:
-            x, y = self.__GetMousePos(event)
-            self.origin_img = self.__origin_bk.copy()
-            cv2.rectangle(self.origin_img,
-                          pt1=(self.__start_x, self.__start_y),
-                          pt2=(x, y),
-                          color=(0, 0, 0),
-                          thickness=1,
-                          lineType=cv2.LINE_4,
-                          shift=0)
-
-            self.Draw()
-
-    def __onScale(self, event):
+    def __onScale(self):
         if self.__proc_flag:
             return
-        else:
-            self.__proc_flag = True
-
-        self.__r_h_1 = self.__scale1.get()
-        self.__g_s_1 = self.__scale2.get()
-        self.__b_v_1 = self.__scale3.get()
-        self.__r_h_2 = self.__scale4.get()
-        self.__g_s_2 = self.__scale5.get()
-        self.__b_v_2 = self.__scale6.get()
+        self.__proc_flag = True
+        self.__get_param_val(self.var.get())
         self.dst_img = self.__inrange()
         self.Draw()
         self.__proc_flag = False
-        pass
+
+    def __show_mode(self, mode):
+        if mode == 0:
+            self.hsv_frame.pack_forget()
+            self.rgb_frame.pack()
+        else:
+            self.rgb_frame.pack_forget()
+            self.hsv_frame.pack()
+            pass
 
     def __inrange(self):
         img_copy = self.origin_img.copy()
 
-        if self.__hsv_flag:
+        if not self.__flag:
+            img_copy = cv2.inRange(img_copy,
+                                   (self.__P3min, self.__P2min, self.__P1min),
+                                   (self.__P3max, self.__P2max, self.__P1max))
+        else:
             img_copy = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV_FULL)
+            H_low = self.__P1min
+            H_high = self.__P1max
+            S_low = self.__P2min
+            S_high = self.__P2max
+            V_low = self.__P3min
+            V_high = self.__P3max
 
-        img = cv2.inRange(img_copy,
-                          (self.__b_v_1, self.__g_s_1, self.__r_h_1),
-                          (self.__b_v_2, self.__g_s_2, self.__r_h_2))
-        return img
+            if H_low > H_high:
+                lower_color1 = np.array([H_low, S_low, V_low])
+                upper_color1 = np.array([255, S_high, V_high])
+                # 指定した範囲内を白、それ以外を黒にする（マスク画像の生成
+                mask1 = cv2.inRange(img_copy, lower_color1, upper_color1)
+                # 取得する色の範囲を指定する
+                lower_color2 = np.array([0, S_low, V_low])
+                upper_color2 = np.array([H_high, S_high, V_high])
+                # 指定した範囲内を白、それ以外を黒にする（マスク画像の生成
+                mask2 = cv2.inRange(img_copy, lower_color2, upper_color2)
+                # マスクの結合
+                img_copy = mask1 + mask2
+            else:
+                # 取得する色の範囲を指定する
+                lower_color = np.array([H_low, S_low, V_low])
+                upper_color = np.array([H_high, S_high, V_high])
+                # 指定した範囲内を白、それ以外を黒にする（マスク画像の生成
+                img_copy = cv2.inRange(img_copy, lower_color, upper_color)
+        return img_copy
 
     def get_data(self):
         param = []
-        param.append(self.__r_h_1)
-        param.append(self.__g_s_1)
-        param.append(self.__b_v_1)
-        param.append(self.__r_h_2)
-        param.append(self.__g_s_2)
-        param.append(self.__b_v_2)
-        param.append(self.__hsv_flag)
-        img = cv2.cvtColor(self.dst_img, cv2.COLOR_GRAY2BGR)
+        param.append(self.__P1min)
+        param.append(self.__P1max)
+        param.append(self.__P2min)
+        param.append(self.__P2max)
+        param.append(self.__P3min)
+        param.append(self.__P3max)
+        param.append(self.__flag)
+
+        img = self.dst_img
+        if len(self.dst_img.shape) == 2:
+            img = cv2.cvtColor(self.dst_img, cv2.COLOR_GRAY2BGR)
         print('Proc : inRange')
         print(f'param = {param}')
         return param, img
@@ -272,7 +233,8 @@ if __name__ == "__main__":
     # img = cv2.imread('./0000_img/I.jpg')
     img = cv2.imread('./0000_img/opencv_logo.jpg')
     param = []
-    param = [150, 188, 45, 255, 255, 109, True]
+    # param = [0, 161, 184, 255, 0, 255, False]   # RGB
+    # param = [191, 49, 191, 255, 191, 255, True]  # HSV
     app = InRange(img, param, gui=True)
     param, dst_img = app.get_data()
     cv2.imwrite('./inrange.jpg', dst_img)
