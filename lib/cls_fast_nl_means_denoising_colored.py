a42dc85675ff0b596fb11d2fb67f4ca4cecc6c85
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-19 07:48:09 +0900

    Update cls_fast_nl_means_denoising_colored.py

--- lib/cls_fast_nl_means_denoising_colored.py
+++ lib/cls_fast_nl_means_denoising_colored.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 
 from lib.gui.cls_edit_window import EditWindow
+from lib.parts.parts_scale import Parts_Scale
 
 
 class FastNlMeansDenoisingColored(EditWindow):
@@ -13,7 +14,8 @@ class FastNlMeansDenoisingColored(EditWindow):
         self.__hColor = 20
         self.__templateWindowSize = 5
         self.__searchWindowSize = 20
-        self.__proc_flag = False
+        self.__proc_flag = True
+        self.__gui = gui
 
         if len(param) == 4:
             self.__h = param[0]
@@ -25,48 +27,47 @@ class FastNlMeansDenoisingColored(EditWindow):
             super().__init__(img, master)
             self.__init_gui()
             self.__init_events()
+
+        self.dst_img = self.__fast_nl_means_denoising_colored()
+
+        if gui:
+            self.Draw()
+            self.__proc_flag = False
             self.run()
-        else:
-            self.dst_img = self.__fast_nl_means_denoising_colored()
 
     def __init_gui(self):
         self.none_label.destroy()
 
-        self.__scale1 = tk.Scale(self.settings_frame)
-        self.__scale1.configure(from_=0, to=20,
-                                label="h", orient="horizontal", command=self.__onScale)
-        self.__scale1.pack(side="top")
-
-        self.__scale2 = tk.Scale(self.settings_frame)
-        self.__scale2.configure(from_=0, to=20,
-                                label="hColor", orient="horizontal", command=self.__onScale)
-        self.__scale2.pack(side="top")
-
-        self.__scale3 = tk.Scale(self.settings_frame)
-        self.__scale3.configure(from_=0, to=10,
-                                label="templateWindowSize", orient="horizontal", command=self.__onScale)
-        self.__scale3.pack(side="top")
-
-        self.__scale4 = tk.Scale(self.settings_frame)
-        self.__scale4.configure(from_=0, to=30,
-                                label="searchWindowSize", orient="horizontal", command=self.__onScale)
-        self.__scale4.pack(side="top")
+        self.__scale1 = Parts_Scale(self.settings_frame)
+        self.__scale1.configure(label='h', side='top', from_=1, to=20)
+        self.__scale2 = Parts_Scale(self.settings_frame)
+        self.__scale2.configure(label='hColor', side='top', from_=1, to=20)
+        self.__scale3 = Parts_Scale(self.settings_frame)
+        self.__scale3.configure(
+            label='templateWindowSize', side='top', from_=1, to=10)
+        self.__scale4 = Parts_Scale(self.settings_frame)
+        self.__scale4.configure(label='searchWindowSize',
+                                side='top', from_=1, to=10)
 
         self.__scale1.set(self.__h)
         self.__scale2.set(self.__hColor)
         self.__scale3.set(self.__templateWindowSize)
         self.__scale4.set(self.__searchWindowSize)
-        pass
 
     def __init_events(self):
-        pass
+        self.__scale1.bind(changed=self.__onScale)
+        self.__scale2.bind(changed=self.__onScale)
+        self.__scale3.bind(changed=self.__onScale)
+        self.__scale4.bind(changed=self.__onScale)
 
-    def __onScale(self, events):
+    def __onScale(self):
         if self.__proc_flag:
             return
-        else:
-            self.__proc_flag = True
+        self.__proc_flag = True
+        self.__wait_msg()
+        self.settings_frame.after(10, self.__onScale_proc)
 
+    def __onScale_proc(self):
         self.__h = self.__scale1.get()
         self.__hColor = self.__scale2.get()
         self.__templateWindowSize = self.__scale3.get()
@@ -74,7 +75,18 @@ class FastNlMeansDenoisingColored(EditWindow):
         self.dst_img = self.__fast_nl_means_denoising_colored()
         self.Draw()
         self.__proc_flag = False
-        pass
+
+    def __wait_msg(self):
+        h, _, _ = self.dst_img.shape
+        self.dst_img = cv2.putText(self.dst_img,
+                                   'wait...',
+                                   (0, int(h/2)),
+                                   cv2.FONT_HERSHEY_PLAIN,
+                                   h/73,
+                                   (0, 0, 255),
+                                   int(h/300),
+                                   cv2.LINE_AA)
+        self.Draw()
 
     def __fast_nl_means_denoising_colored(self):
         img = cv2.fastNlMeansDenoisingColored(
@@ -92,15 +104,18 @@ class FastNlMeansDenoisingColored(EditWindow):
         param.append(self.__hColor)
         param.append(self.__templateWindowSize)
         param.append(self.__searchWindowSize)
-        print('Proc : FastNlMeansDenoisingColored')
-        print(f'param = {param}')
+        if self.__gui:
+            print('Proc : FastNlMeansDenoisingColored')
+            print(f'param = {param}')
         return param, self.dst_img
 
 
 if __name__ == "__main__":
-    img = cv2.imread('./0000_img/I.jpg')
+    # img = cv2.imread('./0000_img/I.jpg')
+    img = cv2.imread('./0000_img/opencv_logo.jpg')
+    # img = cv2.imread('./0000_img/ECU/ECUlow_1.jpg')
     param = []
-    param = [10, 20, 5, 20]
+    param = [10, 20, 5, 10]
     app = FastNlMeansDenoisingColored(img, param, gui=True)
     param, dst_img = app.get_data()
     cv2.imwrite('./FastNlMeansDenoisingColored.jpg', dst_img)
