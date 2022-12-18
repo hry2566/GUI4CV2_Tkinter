d5eebb87660b6a73d29dadf3dbf58fa53aa3a1ba
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-19 07:47:47 +0900

    Update cls_blur.py

--- lib/cls_blur.py
+++ lib/cls_blur.py
@@ -12,6 +12,7 @@ class Blur(EditWindow):
         self.__kernel_x = 1
         self.__kernel_y = 1
         self.__proc_flag = False
+        self.__gui = gui
 
         if len(param) == 2:
             self.__kernel_x = param[0]
@@ -63,8 +64,9 @@ class Blur(EditWindow):
         param = []
         param.append(self.__kernel_x)
         param.append(self.__kernel_y)
-        print('Proc : Blur')
-        print(f'param = {param}')
+        if self.__gui:
+            print('Proc : Blur')
+            print(f'param = {param}')
         return param, self.dst_img
 
 
