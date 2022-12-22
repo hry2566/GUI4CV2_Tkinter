79e061de930283e9a6d76bb6ebb463ea32421ab0
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-22 11:46:13 +0900

    Update cls_bitwise_not.py

--- lib/cls_bitwise_not.py
+++ lib/cls_bitwise_not.py
@@ -1,16 +1,15 @@
-import tkinter as tk
-
+"""色反転"""
 import cv2
-import numpy as np
 
 from lib.gui.cls_edit_window import EditWindow
 
 
-class Bitwise_Not(EditWindow):
+class BitwiseNot(EditWindow):
+    """色反転クラス"""
+
     def __init__(self, img, param, master=None, gui=False):
         self.origin_img = img
         self.__gui = gui
-        # self.__proc_flag = False
 
         if len(param) == 0:
             pass
@@ -25,25 +24,28 @@ class Bitwise_Not(EditWindow):
         self.dst_img = self.__bitwise_not()
 
         if gui:
-            self.Draw()
+            self.draw()
             self.run()
 
     def __init_gui(self):
         self.none_label.destroy()
-        pass
 
     def __init_events(self):
         pass
 
-    def __onScale(self, events):
-        pass
+    # def __onScale(self, events):
+    #     pass
 
     def __bitwise_not(self):
         img_copy = self.origin_img.copy()
         img = cv2.bitwise_not(img_copy)
         return img
 
+    def dummy(self):
+        """パブリックダミー関数"""
+
     def get_data(self):
+        """パラメータ取得"""
         param = []
         if self.__gui:
             print('Proc : Bitwise_Not')
@@ -51,9 +53,9 @@ class Bitwise_Not(EditWindow):
         return param, self.dst_img
 
 
-if __name__ == "__main__":
-    img = cv2.imread('./0000_img/I.jpg')
-    param = []
-    app = Bitwise_Not(img, param, gui=True)
-    param, dst_img = app.get_data()
-    cv2.imwrite('./Bitwise_Not.jpg', dst_img)
+# if __name__ == "__main__":
+#     img = cv2.imread('./0000_img/I.jpg')
+#     param = []
+#     app = BitwiseNot(img, param, gui=True)
+#     param, dst_img = app.get_data()
+#     cv2.imwrite('./Bitwise_Not.jpg', dst_img)
