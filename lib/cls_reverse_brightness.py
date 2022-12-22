fa5126c45f7cf81a4e79588c8d8e0aca965f5d1f
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-22 11:47:10 +0900

    Update cls_reverse_brightness.py

--- lib/cls_reverse_brightness.py
+++ lib/cls_reverse_brightness.py
@@ -1,15 +1,15 @@
-import tkinter as tk
-
+"""明度反転"""
 import cv2
-import numpy as np
 
 from lib.gui.cls_edit_window import EditWindow
 
 
 class ReverseBrightness(EditWindow):
+    """明度反転クラス"""
+
     def __init__(self, img, param, master=None, gui=False):
         self.origin_img = img
-        self.__gui=gui
+        self.__gui = gui
 
         if gui:
             super().__init__(img, master)
@@ -26,32 +26,35 @@ class ReverseBrightness(EditWindow):
         self.dst_img = self.__reverse_brightness()
 
         if gui:
-            self.Draw()
+            self.draw()
             self.run()
 
     def __init_gui(self):
         self.none_label.destroy()
-        pass
 
     def __init_events(self):
         pass
 
-    def __onClick(self, event):
-        self.Draw()
-        pass
+    # def __on_click(self, event):
+    #     pass
 
-    def __onScale(self, events):
-        pass
+    # def __on_scale(self, events):
+    #     pass
 
     def __reverse_brightness(self):
         img_copy = self.origin_img.copy()
-        h1, s1, v1 = cv2.split(cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV))
-        v1 = 255 - v1
-        img = cv2.cvtColor(cv2.merge((h1, s1, v1)), cv2.COLOR_HSV2BGR)
-
+        hue, saturation, value = cv2.split(
+            cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV))
+        value = 255-value
+        img = cv2.cvtColor(
+            cv2.merge((hue, saturation, value)), cv2.COLOR_HSV2BGR)
         return img
 
+    def dummy(self):
+        """パブリックダミー関数"""
+
     def get_data(self):
+        """パラメータ取得"""
         param = []
         if self.__gui:
             print('Proc : ReverseBrightness')
@@ -59,9 +62,9 @@ class ReverseBrightness(EditWindow):
         return param, self.dst_img
 
 
-if __name__ == "__main__":
-    img = cv2.imread('./0000_img/test.jpg')
-    param = [False, False, True]
-    app = ReverseBrightness(img, param, gui=True)
-    param, dst_img = app.get_data()
-    cv2.imwrite('./ReverseBrightness.jpg', dst_img)
+# if __name__ == "__main__":
+#     img = cv2.imread('./0000_img/test.jpg')
+#     param = [False, False, True]
+#     app = ReverseBrightness(img, param, gui=True)
+#     param, dst_img = app.get_data()
+#     cv2.imwrite('./ReverseBrightness.jpg', dst_img)
