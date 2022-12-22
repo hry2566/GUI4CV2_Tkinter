a0fd84501bce399ecd839f942d9811320affb9f5
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-22 11:46:58 +0900

    Update cls_median_blur.py

--- lib/cls_median_blur.py
+++ lib/cls_median_blur.py
@@ -1,13 +1,13 @@
-import os
-import tkinter as tk
-
+"""メディアンぼかし"""
 import cv2
 
 from lib.gui.cls_edit_window import EditWindow, even2odd
 from lib.parts.parts_scale import Parts_Scale
 
 
-class Median_Blur(EditWindow):
+class MedianBlur(EditWindow):
+    """メディアンぼかしクラス"""
+
     def __init__(self, img, param, master=None, gui=False):
         self.origin_img = img
         self.__kernel = 1
@@ -25,7 +25,7 @@ class Median_Blur(EditWindow):
         self.dst_img = self.__median_blur()
 
         if gui:
-            self.Draw()
+            self.draw()
             self.run()
 
     def __init_gui(self):
@@ -36,19 +36,16 @@ class Median_Blur(EditWindow):
         self.__scale.set(self.__kernel)
 
     def __init_events(self):
-        self.__scale.bind(changed=self.__onScale)
+        self.__scale.bind(changed=self.__on_scale)
 
-    def __onScale(self):
+    def __on_scale(self):
         if self.__proc_flag:
             return
-        else:
-            self.__proc_flag = True
-
+        self.__proc_flag = True
         self.__kernel = self.__scale.get()
         self.dst_img = self.__median_blur()
-        self.Draw()
+        self.draw()
         self.__proc_flag = False
-        pass
 
     def __median_blur(self):
         img_copy = self.origin_img.copy()
@@ -57,7 +54,11 @@ class Median_Blur(EditWindow):
         img = cv2.medianBlur(img_copy, self.__kernel)
         return img
 
+    def dummy(self):
+        """パブリックダミー関数"""
+
     def get_data(self):
+        """パラメータ取得"""
         param = []
         param.append(self.__kernel)
         if self.__gui:
@@ -66,10 +67,10 @@ class Median_Blur(EditWindow):
         return param, self.dst_img
 
 
-if __name__ == "__main__":
-    img = cv2.imread('./0000_img/opencv_logo.jpg')
-    param = []
-    param = [8]
-    app = Median_Blur(img, param, gui=True)
-    param, dst_img = app.get_data()
-    cv2.imwrite('./Median_Blur.jpg', dst_img)
+# if __name__ == "__main__":
+#     img = cv2.imread('./0000_img/opencv_logo.jpg')
+#     param = []
+#     param = [8]
+#     app = MedianBlur(img, param, gui=True)
+#     param, dst_img = app.get_data()
+#     cv2.imwrite('./Median_Blur.jpg', dst_img)
