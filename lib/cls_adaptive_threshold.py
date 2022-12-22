bd3d04aa208cdc68a65b18c2a2bfe019dada4d54
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-22 11:46:07 +0900

    Update cls_adaptive_threshold.py

--- lib/cls_adaptive_threshold.py
+++ lib/cls_adaptive_threshold.py
@@ -1,3 +1,4 @@
+"""二値化(AdaptiveThresholed)"""
 import tkinter as tk
 
 import cv2
@@ -6,21 +7,23 @@ from lib.gui.cls_edit_window import EditWindow, even2odd
 from lib.parts.parts_scale import Parts_Scale
 
 
-class Adaptive_Thresholed(EditWindow):
+class AdaptiveThresholed(EditWindow):
+    """二値化(AdaptiveThresholed)クラス"""
+
     def __init__(self, img, param, master=None, gui=False):
         self.origin_img = img
-        self.__method_index = 0
-        self.__Value = 255
+        self.__methodindex = 0
+        self.__value = 255
         self.__block_size = 3
         self.__c = 1
         self.__proc_flag = False
-        self.__adaptiveMethod = [cv2.ADAPTIVE_THRESH_MEAN_C,
+        self.__adaptivemethod = [cv2.ADAPTIVE_THRESH_MEAN_C,
                                  cv2.ADAPTIVE_THRESH_GAUSSIAN_C]
         self.__gui = gui
 
         if len(param) == 4:
-            self.__method_index = param[0]
-            self.__Value = param[1]
+            self.__methodindex = param[0]
+            self.__value = param[1]
             self.__block_size = param[2]
             self.__c = param[3]
 
@@ -32,13 +35,13 @@ class Adaptive_Thresholed(EditWindow):
         self.dst_img = self.__adaptive_thresholed()
 
         if gui:
-            self.Draw()
+            self.draw()
             self.run()
 
     def __init_gui(self):
         self.none_label.destroy()
 
-        if self.__method_index == 0:
+        if self.__methodindex == 0:
             val = 'MEAN'
         else:
             val = 'GAUSSIAN'
@@ -46,57 +49,49 @@ class Adaptive_Thresholed(EditWindow):
         self.__tkvar = tk.StringVar(value=val)
         __values = ['MEAN', 'GAUSSIAN']
         self.__optionmenu2 = tk.OptionMenu(
-            self.settings_frame, self.__tkvar, *__values, command=self.__onSelect)
+            self.settings_frame, self.__tkvar, *__values, command=self.__on_select)
         self.__optionmenu2.pack(fill='x', side='top')
-
         self.__scale1 = Parts_Scale(self.settings_frame)
         self.__scale1.configure(label='value', side='top', from_=1, to=255)
-
         self.__scale2 = Parts_Scale(self.settings_frame)
         self.__scale2.configure(
-            label='block_size', side='top', from_=1, to=255)
-
+            label='block_size', side='top', from_=2, to=255)
         self.__scale3 = Parts_Scale(self.settings_frame)
         self.__scale3.configure(label='c', side='top', from_=1, to=255)
 
-        self.__scale1.set(self.__Value)
+        self.__scale1.set(self.__value)
         self.__scale2.set(self.__block_size)
         self.__scale3.set(self.__c)
 
     def __init_events(self):
-        self.__scale1.bind(changed=self.__onScale)
-        self.__scale2.bind(changed=self.__onScale)
-        self.__scale3.bind(changed=self.__onScale)
-        pass
+        self.__scale1.bind(changed=self.__on_scale)
+        self.__scale2.bind(changed=self.__on_scale)
+        self.__scale3.bind(changed=self.__on_scale)
 
-    def __onSelect(self, event):
+    def __on_select(self, event):
         if self.__proc_flag:
             return
-        else:
-            self.__proc_flag = True
+        self.__proc_flag = True
 
         if self.__tkvar.get() == 'MEAN':
-            self.__method_index = 0
+            self.__methodindex = 0
         elif self.__tkvar.get() == 'GAUSSIAN':
-            self.__method_index = 1
+            self.__methodindex = 1
 
         self.dst_img = self.__adaptive_thresholed()
-        self.Draw()
+        self.draw()
         self.__proc_flag = False
 
-    def __onScale(self):
+    def __on_scale(self):
         if self.__proc_flag:
             return
-        else:
-            self.__proc_flag = True
-
-        self.__Value = self.__scale1.get()
+        self.__proc_flag = True
+        self.__value = self.__scale1.get()
         self.__block_size = self.__scale2.get()
         self.__c = self.__scale3.get()
         self.dst_img = self.__adaptive_thresholed()
-        self.Draw()
+        self.draw()
         self.__proc_flag = False
-        pass
 
     def __adaptive_thresholed(self):
         img_copy = self.origin_img.copy()
@@ -105,31 +100,35 @@ class Adaptive_Thresholed(EditWindow):
         self.__block_size = int(even2odd(self.__block_size))
 
         img_copy = cv2.adaptiveThreshold(img_copy,
-                                         self.__Value,
-                                         self.__adaptiveMethod[self.__method_index],
+                                         self.__value,
+                                         self.__adaptivemethod[self.__methodindex],
                                          cv2.THRESH_BINARY,
                                          self.__block_size,
                                          self.__c)
         return img_copy
 
+    def dummy(self):
+        """パブリックダミー関数"""
+
     def get_data(self):
+        """パラメータ取得"""
         param = []
-        param.append(self.__method_index)
-        param.append(self.__Value)
+        param.append(self.__methodindex)
+        param.append(self.__value)
         param.append(self.__block_size)
         param.append(self.__c)
         img = cv2.cvtColor(self.dst_img, cv2.COLOR_GRAY2BGR)
         if self.__gui:
-            print('Proc : Average')
+            print('Proc : AdaptiveThresholed')
             print(f'param = {param}')
         return param, img
 
 
-if __name__ == "__main__":
-    # img = cv2.imread('./0000_img/opencv_logo.jpg')
-    img = cv2.imread('./0000_img/ECU/ECUlow_1.jpg')
-    param = []
-    param = [1, 255, 33, 31]
-    app = Adaptive_Thresholed(img, param, gui=True)
-    param, dst_img = app.get_data()
-    cv2.imwrite('./adaptive_thresholed.jpg', dst_img)
+# if __name__ == "__main__":
+#     # img = cv2.imread('./0000_img/opencv_logo.jpg')
+#     img = cv2.imread('./0000_img/ECU/ECUlow_1.jpg')
+#     param = []
+#     param = [1, 255, 33, 31]
+#     app = AdaptiveThresholed(img, param, gui=True)
+#     param, dst_img = app.get_data()
+#     cv2.imwrite('./AdaptiveThresholed.jpg', dst_img)
