0590d69a648fc70a407547b5276637304f349f18
hry2566 <86605611+hry2566@users.noreply.github.com>
2022-12-22 11:46:11 +0900

    Update cls_bilateral_filter.py

--- lib/cls_bilateral_filter.py
+++ lib/cls_bilateral_filter.py
@@ -1,3 +1,4 @@
+"""ぼかし(BilateralFilter)"""
 import tkinter as tk
 
 import cv2
@@ -5,7 +6,9 @@ import cv2
 from lib.gui.cls_edit_window import EditWindow
 
 
-class Bilateral_Filter(EditWindow):
+class BilateralFilter(EditWindow):
+    """ぼかし(BilateralFilter)クラス"""
+
     def __init__(self, img, param, master=None, gui=False):
         self.origin_img = img
         self.__d = 0
@@ -32,36 +35,33 @@ class Bilateral_Filter(EditWindow):
 
         self.__scale1 = tk.Scale(self.settings_frame)
         self.__scale1.configure(from_=-128, to=128,
-                                label="d", orient="horizontal", command=self.__onScale)
+                                label="d", orient="horizontal", command=self.__on_scale)
         self.__scale1.pack(side="top")
         self.__scale2 = tk.Scale(self.settings_frame)
         self.__scale2.configure(from_=0, to=50,
-                                label="sigma_color", orient="horizontal", command=self.__onScale)
+                                label="sigma_color", orient="horizontal", command=self.__on_scale)
         self.__scale2.pack(side="top")
         self.__scale3 = tk.Scale(self.settings_frame)
         self.__scale3.configure(from_=0, to=50,
-                                label="sigma_space", orient="horizontal", command=self.__onScale)
+                                label="sigma_space", orient="horizontal", command=self.__on_scale)
         self.__scale3.pack(side="top")
 
         self.__scale1.set(self.__d)
         self.__scale2.set(self.__sigma_color)
         self.__scale3.set(self.__sigma_space)
-        pass
 
     def __init_events(self):
         pass
 
-    def __onScale(self, events):
+    def __on_scale(self, events):
         if self.__proc_flag:
             return
         self.__proc_flag = True
-
         self.__d = self.__scale1.get()
         self.__sigma_color = self.__scale2.get()
         self.__sigma_space = self.__scale3.get()
-
         self.dst_img = self.__bilateral_filter()
-        self.Draw()
+        self.draw()
         self.__proc_flag = False
 
     def __bilateral_filter(self):
@@ -73,22 +73,26 @@ class Bilateral_Filter(EditWindow):
                                   sigmaSpace=self.__sigma_space)
         return img
 
+    def dummy(self):
+        """パブリックダミー関数"""
+
     def get_data(self):
+        """パラメータ取得"""
         param = []
         param.append(self.__d)
         param.append(self.__sigma_color)
         param.append(self.__sigma_space)
         if self.__gui:
-            print('Proc : Bilateral_Filter')
+            print('Proc : BilateralFilter')
             print(f'param = {param}')
         return param, self.dst_img
 
 
-if __name__ == "__main__":
-    # img = cv2.imread('./0000_img/opencv_logo.jpg')
-    img = cv2.imread('./0000_img/I.jpg')
-    param = []
-    param = [60, 30, 30]
-    app = Bilateral_Filter(img, param, gui=True)
-    param, dst_img = app.get_data()
-    cv2.imwrite('./bilateral_filter.jpg', dst_img)
+# if __name__ == "__main__":
+#     # img = cv2.imread('./0000_img/opencv_logo.jpg')
+#     img = cv2.imread('./0000_img/I.jpg')
+#     param = []
+#     param = [60, 30, 30]
+#     app = BilateralFilter(img, param, gui=True)
+#     param, dst_img = app.get_data()
+#     cv2.imwrite('./BilateralFilter.jpg', dst_img)
